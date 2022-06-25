7e5a659daee9d18c1a466c359eebff44c0880a2f
machearn <machearn.n@gmail.com>
2022-06-25 17:40:19 -0400

    fix: adapt new config for dap-ui

--- lua/user/dap.lua
+++ lua/user/dap.lua
@@ -20,20 +20,57 @@ dap_install.config("codelldb", {})
 -- add other configs here
 
 dapui.setup {
-  sidebar = {
-    elements = {
-      {
-        id = "scopes",
-        size = 0.25, -- Can be float or integer > 1
+  icons = { expanded = "▾", collapsed = "▸" },
+  mappings = {
+    -- Use a table to apply multiple mappings
+    expand = { "<CR>", "<2-LeftMouse>" },
+    open = "o",
+    remove = "d",
+    edit = "e",
+    repl = "r",
+    toggle = "t",
+  },
+  -- Expand lines larger than the window
+  -- Requires >= 0.7
+  expand_lines = vim.fn.has("nvim-0.7"),
+  -- Layouts define sections of the screen to place windows.
+  -- The position can be "left", "right", "top" or "bottom".
+  -- The size specifies the height/width depending on position.
+  -- Elements are the elements shown in the layout (in order).
+  -- Layouts are opened in order so that earlier layouts take priority in window sizing.
+  layouts = {
+    {
+      elements = {
+        -- Elements can be strings or table with id and size keys.
+        { id = "scopes", size = 0.4 },
+        { id = "stacks", size = 0.3 },
+        { id = "watches", size = 0.2 },
+        { id = "breakpoints", size = 0.1 },
       },
-      { id = "breakpoints", size = 0.25 },
+      size = 50,
+      position = "left",
     },
-    size = 40,
-    position = "right", -- Can be "left", "right", "top", "bottom"
+    {
+      elements = {
+        "repl",
+        "console",
+      },
+      size = 10,
+      position = "bottom",
+    },
   },
-  tray = {
-    elements = {},
+  floating = {
+    max_height = nil, -- These can be integers or a float between 0 and 1.
+    max_width = nil, -- Floats will be treated as percentage of your screen.
+    border = "single", -- Border style. Can be "single", "double" or "rounded"
+    mappings = {
+      close = { "q", "<Esc>" },
+    },
   },
+  windows = { indent = 1 },
+  render = {
+    max_type_length = nil, -- Can be integer or nil.
+  }
 }
 
 vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DiagnosticSignError", linehl = "", numhl = "" })
@@ -65,8 +102,8 @@ dap.adapters.codelldb = function(on_adapter)
   local stdout = vim.loop.new_pipe(false)
   local stderr = vim.loop.new_pipe(false)
   local opts = {
-    stdio = {nil, stdout, stderr},
-    args = {'--port', tostring(port)},
+    stdio = { nil, stdout, stderr },
+    args = { '--port', tostring(port) },
   }
   local handle
   local pid_or_err
