40244b5e4b04ad4df047bdd0fbbf4b37f8858d95
machearn <machearn.n@gmail.com>
2023-11-26 13:28:55 -0500

    fix: use pcall to import nvim-tree
    The actual bug of nvim-tree not correctly setting up is the failure of import nvim-tree-config

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -75,7 +75,12 @@ local function on_attach(bufnr)
 
 end
 
-require("nvim-tree").setup {
+local status_ok, nvim_tree = pcall(require, "nvim-tree")
+if not status_ok then
+  return
+end
+
+nvim_tree.setup {
   on_attach = on_attach,
   update_focused_file = {
     enable = true,
