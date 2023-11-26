045e085e2d0c6336df359b263d7497e0589da7c7
machearn <machearn.n@gmail.com>
2023-11-25 22:44:28 -0500

    refactor: use `require` to disable `lazy load`

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -75,19 +75,7 @@ local function on_attach(bufnr)
 
 end
 
-local status_ok, nvim_tree = pcall(require, "nvim-tree")
-if not status_ok then
-  return
-end
-
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
-  return
-end
-
-local tree_cb = nvim_tree_config.nvim_tree_callback
-
-nvim_tree.setup {
+require("nvim-tree").setup {
   on_attach = on_attach,
   update_focused_file = {
     enable = true,
