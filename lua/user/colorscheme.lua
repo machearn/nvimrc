97a6a66bbffc82eb7d363e43948fb62b53829e5a
machearn <machearn.n@gmail.com>
2023-12-02 14:18:47 -0500

    ui: change colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,5 +1,4 @@
-local colorscheme = "tokyonight"
-vim.g.tokyonight_style = "night"
+local colorscheme = "sonokai"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
